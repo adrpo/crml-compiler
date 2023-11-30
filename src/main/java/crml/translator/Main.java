12f5c3db73be373e1665e512026769e294f0c875
Adrian Pop <adrian.pop@liu.se>
2023-11-30 11:06:57 +0100

    fix comand line

--- src/main/java/crml/translator/Main.java
+++ src/main/java/crml/translator/Main.java
@@ -22,28 +22,46 @@ import org.apache.logging.log4j.LogManager;
 
 public class Main {
 
-   private static final Logger logger = LogManager.getLogger();	
+  private static final Logger logger = LogManager.getLogger();	
 
-   public static void main( String[] args ) throws Exception {
-
-    if (args[0] == null)
-    {
+  public static void showUsage () {
       System.out.println("usage: crml.translator.Test path/to/tests/[test.crml] [-o /path/to/output]");
       System.out.println("       provide an input directory or a test to translate all or the given .crml files to .mo files");
       System.out.println("       provide an output directory via -o on where to write the .mo files");
       System.out.println("       if no output directory via -o is given then the .mo files are generated in the current directory");
-      return;
-    }
-    String path = new File(args[0]).getCanonicalPath();
-    logger.trace("Directory for tests: " + path);
+      System.out.flush();
+  }
 
+
+   public static void main( String[] args ) throws Exception {
+    logger.trace("Args: " + args.length);
+    for (int i = 0; i < args.length; i++) {
+      logger.trace("Arg: [" + i + "]: " + args[i]);
+    }
+
+    String outPath = null;
+
+    if (args.length == 0 || args.length == 2) { showUsage(); return; }
+    if (args[0] == null) { showUsage(); return; }
+    // check for -o directory
+    if (args.length == 3) {
+      if (args[1].equals("-o"))
+      {
+        if (args[2] == null) { showUsage(); return; }
+        outPath = args[2];
+      }
+    }
+    
+    String path = new File(args[0]).getCanonicalPath();
     File file = new File ( path );
-    if (file.isDirectory()) {
-    String tests[] = file.list();
 
     File out_dir = new File("generated");
     out_dir.mkdir();
     logger.trace("Directory for generated .mo files: " + out_dir.getPath());
+
+    if (file.isDirectory()) {
+    logger.trace("File for test: " + path);
+    String tests[] = file.list();
 
     for (String test : tests) {
     	if(test.endsWith(".crml")) {
@@ -52,12 +70,12 @@ public class Main {
     	}
     }
     } else if (file.isFile()) {
-    	File out_dir = new File("generated");
-        out_dir.mkdir();
-        logger.trace("Directory for generated .mo files: " + out_dir.getPath());
+      logger.trace("Directory for tests: " + path);
+      out_dir.mkdir();
+      logger.trace("Directory for generated .mo files: " + out_dir.getPath());
         
-        logger.trace("Translating: " + file);
-		parse_file("", path, out_dir.getPath(), false, false);
+      logger.trace("Translating: " + file);
+		  parse_file("", path, out_dir.getPath(), false, false);
     } else {
     	logger.error("Translation error: " + path +  " is not a correct path");
     }
@@ -96,7 +114,7 @@ public class Main {
       
         if (result != null) {  	
         
-          File out_file = new File(gen_dir + java.io.File.separator + Utilities.stripNameEnding(file)+ ".mo");  
+          File out_file = new File(gen_dir + java.io.File.separator + Utilities.stripNameEnding(file) + ".mo");  
         
           out_file.getParentFile().mkdirs();   	
         
@@ -106,7 +124,7 @@ public class Main {
           logger.trace("Translated: " + file);
 
           if(generateExternal && !external_var.isEmpty()){
-            File ext_file = new File(gen_dir + java.io.File.separator + Utilities.stripNameEnding(file)+ "_external.txt");
+            File ext_file = new File(gen_dir + java.io.File.separator + Utilities.stripNameEnding(file) + "_external.txt");
             BufferedWriter ext_writer = new BufferedWriter(new FileWriter(ext_file));
             logger.trace("External variables saved in: " + ext_file);
 
--- src/main/java/crml/translator/Utilities.java
+++ src/main/java/crml/translator/Utilities.java
@@ -1,5 +1,8 @@
 package crml.translator;
 
+import java.io.File;
+import java.nio.file.Path;
+
 public class Utilities {
 
     /**
@@ -7,10 +10,34 @@ public class Utilities {
      * @return
      */
     public static String stripNameEnding (String name){
+        // on Windows you cannot just append the path without 
+        // extension because it contains the drive letter
+        // so just remove that!
+        if (OS.isWindows()) {
+          File file = new File(name);
+          Path path = file.toPath();
+          path = path.getRoot().relativize(path);
+          name = path.toString();
+        }
         return name.substring(0, name.lastIndexOf('.'));
     }
 
    public static String addDirToPath (String path, String dir){
         return path + java.io.File.separator + dir;
     }
+
+    public static final class OS
+    {
+       private static String OSname = null;
+       public static String name()
+       {
+          if(OSname == null) { OSname = System.getProperty("os.name"); }
+          return OSname;
+       }
+       public static boolean isWindows()
+       {
+          return name().startsWith("Windows");
+       }
+    }
+
 }
